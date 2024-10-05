86fed3733b0826f657e25217100392b497861923
mssalazarb <mssalazarb@outlook.com>
2024-10-05 02:29:37 -0500

    Update global exception

--- src/main/java/com/msf/account/middled/exceptions/GlobalException.java
+++ src/main/java/com/msf/account/middled/exceptions/GlobalException.java
@@ -1,45 +1,21 @@
 package com.msf.account.middled.exceptions;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import feign.FeignException;
-import org.springframework.http.HttpStatus;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import java.util.HashMap;
-import java.util.Map;
-
-//Aqui coje la excepncionpara mostrar al cliente
 @ControllerAdvice
+@RequiredArgsConstructor
 public class GlobalException {
-    private final ObjectMapper objectMapper = new ObjectMapper();
 
-    // Handle general exceptions
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<Map<String, Object>> handleGeneralException(Exception e) {
-        Map<String, Object> errorResponse = new HashMap<>();
-        errorResponse.put("status", HttpStatus.BAD_REQUEST.value());
-        errorResponse.put("message", "Error en la petición: " + e.getMessage());
-
-        return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST)
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(errorResponse);
-    }
-
-    // Handle Feign client exceptions
     @ExceptionHandler(FeignException.class)
-    public ResponseEntity<Map<String, Object>> handleFeignException(FeignException ex) {
-        Map<String, Object> errorResponse = new HashMap<>();
-        errorResponse.put("status", ex.status());
-        errorResponse.put("message", "Error en la comunicación externa: " + ex.getMessage());
-        errorResponse.put("details", ex.contentUTF8());
-
+    public ResponseEntity<?> feignException(FeignException ex) {
         return ResponseEntity
                 .status(ex.status())
                 .contentType(MediaType.APPLICATION_JSON)
-                .body(errorResponse);
+                .body(ex.contentUTF8());
     }
 }
