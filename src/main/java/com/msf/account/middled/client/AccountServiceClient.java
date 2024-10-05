5854056529679214efa0fb57e8448f8db8d267ca
mssalazarb <mssalazarb@outlook.com>
2024-10-05 02:20:35 -0500

    Update account service path

--- src/main/java/com/msf/account/middled/client/AccountServiceClient.java
+++ src/main/java/com/msf/account/middled/client/AccountServiceClient.java
@@ -8,12 +8,12 @@ import org.springframework.web.bind.annotation.*;
 @FeignClient(url = "${microservices.url}/api/v1/account-service" , name = "accountServiceClient")
 public interface AccountServiceClient {
 
-    @PostMapping("/account")
+    @PostMapping("/accounts")
     ResponseEntity<Account> createAccount(@RequestHeader(value = "x-cm-client-request-id", required = true) String xCmClientRequestId,
                                            @RequestHeader(value = "x-cm-client-user-agent", required = true) String xCmClientUserAgent,
                                            @RequestBody Account account);
 
-    @PutMapping("/account/{id}")
+    @PutMapping("/accounts/{id}")
     ResponseEntity<Account> updateAccount(@RequestHeader(value = "x-cm-client-request-id", required = true) String xCmClientRequestId,
                                           @RequestHeader(value = "x-cm-client-user-agent", required = true) String xCmClientUserAgent,
                                           @PathVariable("id") Long id,
